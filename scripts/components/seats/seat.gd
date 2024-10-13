f2199d5774adc80a6ec639f553daa9b2f9f84b14
n8hopp <hopper26@up.edu>
2024-10-13 02:42:32 -0700

    Some basic seat stuff drafted out
    - Player can approach seat & it makes them invisible and deactivates controls, and swaps to seat's camera

---
+++ scripts/components/seats/seat.gd
@@ -0,0 +1,45 @@
+@icon("res://assets/materials/seat.svg")
+class_name Seat
+extends Node3D
+
+## This seat's camera
+@export var main_camera : Camera3D
+
+## The MeshInstance3D that will be highlighted when the player gets
+## close to the seat
+@export var highlight_target : MeshInstance3D
+
+@export var outline_thickness := 0.00025
+var glow_shader = preload("res://assets/materials/outline3D.gdshader")
+
+var enterable := false
+var entered := false
+
+var player : CharacterBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+
+func _on_hover_detector_area_entered(area):
+	highlight_target.set_instance_shader_parameter("thickness", outline_thickness)
+	enterable = true
+
+func _on_hover_detector_area_exited(area):
+	highlight_target.set_instance_shader_parameter("thickness", 0)
+	enterable = false
+	
+func _input(event):
+	if event.is_action_pressed("interact"):
+		if enterable:
+			player.hide()
+			player.process_mode = Node.PROCESS_MODE_DISABLED
+			main_camera.current = true
+			enterable = false
+			entered = true
+		elif entered:
+			player.show()
+			player.process_mode = Node.PROCESS_MODE_INHERIT
+			main_camera.current = false
+			enterable = true
+			entered = false
--- scripts/playground/swingseat.gd
+++ scripts/playground/swingseat.gd
@@ -4,7 +4,7 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_forward"):
 		var force = Vector3(5, 0, 0)
 		apply_central_force(force)
-	if Input.is_action_pressed("move_backward"):
+	if Input.is_action_pressed("move_back"):
 		var force = Vector3(-5, 0, 0)
 		apply_central_force(force)
 	
