5b1985d20b37b260854510491870598457fe28eb
n8hopp <hopper26@up.edu>
2024-10-21 03:43:54 -0700

    SittableRigidbody class & component
    - To use, drag "scene/components/sittable_rigidbody.tscn" into the scene you need it in (if you have non-moving parts that are a part of your model, this would be a Node3D child)
    - Right click on the new node, click "Editable Children" or "Make Local"
    - Right click again, click "Extend Script", then write your needed code for movement of the object (please use carousel and swing as a template! i'll be making slide or monkey bars work to show off an animated version soon)

--- scripts/components/seats/sittable_rigidbody.gd
+++ scripts/components/seats/sittable_rigidbody.gd
@@ -2,14 +2,26 @@
 class_name SittableRigidbody
 extends RigidBody3D
 
-## This seat's camera
-@export var main_camera : Camera3D
+## This seat's player location object, a Node3D
+## when getting in this seat the player will be reparented to this node.
+## this should usually be a child of smoothing
+@export var player_seated_location : Node3D
+var original_player_pos := Vector3.ZERO
+@export var override_exit_behavior := false
+
+## Specifies if the user can freely move their mouse when sitting on this seat
+## NOTE: this is not implemented yet as a toggle as of 10/21/24, default is yes -n8
+@export var free_look : bool
+
+@export_category("Glowing Selection")
+## The Area3D denoting where the player's raycast can collide with this
+@export var hover_detector : Area3D
 
 ## The MeshInstance3D that will be highlighted when the player gets
 ## close to the seat
 @export var highlight_target : MeshInstance3D
+@export var glow_outline_thickness := 0.00025
 
-@export var outline_thickness := 0.00025
 var glow_shader = preload("res://assets/materials/outline3D.gdshader")
 
 var enterable := false
@@ -17,16 +29,24 @@ var entered := false
 
 var player : CharacterBody3D
 
+var player_cam_pos : Vector3
+
 var debug := false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	if player == null:
 		entered = true
+		
+	hover_detector.connect("area_entered", _on_hover_detector_area_entered)
+	hover_detector.connect("area_exited", _on_hover_detector_area_exited)
+	
+	#highlight_target.material_overlay = glow_shader
 
 func _on_hover_detector_area_entered(area):
-	highlight_target.set_instance_shader_parameter("thickness", outline_thickness)
-	enterable = true
+	if !entered:
+		highlight_target.set_instance_shader_parameter("thickness", glow_outline_thickness)
+		enterable = true
 
 func _on_hover_detector_area_exited(area):
 	highlight_target.set_instance_shader_parameter("thickness", 0)
@@ -35,16 +55,52 @@ func _on_hover_detector_area_exited(area):
 func _input(event):
 	if event.is_action_pressed("interact"):
 		if enterable:
-			player.hide()
-			player.process_mode = Node.PROCESS_MODE_DISABLED
-			main_camera.current = true
+			highlight_target.set_instance_shader_parameter("thickness", 0)
 			enterable = false
 			entered = true
+			_toggle_sit(true)
 		elif entered:
-			player.show()
-			player.process_mode = Node.PROCESS_MODE_INHERIT
-	#		player.look_at(global_position + linear_velocity.normalized())
-
-			main_camera.current = false
-			enterable = true
 			entered = false
+			_toggle_sit(false)
+			
+
+## Set the sitting state of the user.
+## Reparents the player to the player_seated_location node
+## runs _custom_exit_behavior if override_exit_behavior is set to true
+func _toggle_sit(state : bool):
+	player.sitting = state
+	if state == true:
+		original_player_pos = player.position
+		
+		player.reparent(player_seated_location)
+		
+		# weird fix for frame smoothing not working when stacked
+		player.camera.reparent(player)
+		player.rotation.x = 0
+		player.rotation.y = 0
+		player.rotation.z = 0
+		
+		player.left_cam_limit = 70
+		player.right_cam_limit = 70
+		player.camera.rotation = Vector3.ZERO
+		player.position = Vector3.ZERO
+	elif state == false:
+		player.camera.reparent(player.smoothing)
+		player.camera.rotation = Vector3.ZERO
+		player.camera.position.x = 0
+		player.camera.position.z = 0
+		
+		player.left_cam_limit = 180
+		player.right_cam_limit = 180
+		player.reparent(get_tree().root)
+		
+		if !override_exit_behavior:
+			player.position = original_player_pos
+		else:
+			_custom_exit_behavior()
+
+## OVERRIDE IN CHILDREN: allows you to specify custom behavior for what happens
+## on player leaving this seat. I.e., swings give the player their velocity 
+## and position on exit
+func _custom_exit_behavior():
+	pass
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -7,13 +7,22 @@ const JUMP_VELOCITY = 4.5
 @export var mouse_sens := 0.002
 
 @onready var camera = $Smoothing/Camera3D
+@onready var smoothing = $Smoothing
+
+var left_cam_limit = 180
+var right_cam_limit = 180
 var thrown := false
+
+var sitting : bool = false
 
 func _ready():
 	# locks mouse to window
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _physics_process(delta):
+	if sitting:
+		return
+		
 	# Add the gravity.
 	if is_on_floor():
 		thrown = false
@@ -41,6 +50,8 @@ func _physics_process(delta):
 func _input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * mouse_sens)
+		rotation.y = clampf(rotation.y, -deg_to_rad(left_cam_limit), deg_to_rad(right_cam_limit))
+
 		camera.rotate_x(-event.relative.y * mouse_sens)
 		camera.rotation.x = clampf(camera.rotation.x, -deg_to_rad(70), deg_to_rad(70))
 	
