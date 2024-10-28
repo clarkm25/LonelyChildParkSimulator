15bb03aed04676dfb720c715cfd63dfe42c0fedb
n8hopp <hopper26@up.edu>
2024-10-28 11:08:02 -0700

    Carousel now plays a qte on-enter always

--- scripts/playground/carousel_seat.gd
+++ scripts/playground/carousel_seat.gd
@@ -1,15 +1,27 @@
 extends SittableRigidbody
-
 var amount_spins := 0.0
+
+@onready var qte = $"../QTE"
 func _physics_process(delta):
 	if entered:
 		var raycast_collision = $"../QTE/CenterChecker".get_collider()
 		if raycast_collision:
 			if amount_spins >= (angular_velocity.y):
-				$"../QTE".play()
+				qte.play()
 				amount_spins = 0.0
 			else:
 				amount_spins = amount_spins + 1.0
+				
+## Set the sitting state of the user.
+## Reparents the player to the player_seated_location node
+## runs _custom_exit_behavior if override_exit_behavior is set to true
+func _toggle_sit(state : bool):
+	super(state)
+	if state == true and !qte.playing:
+		qte.play()
+	if state == false:
+		qte.stop()
+
 func _custom_exit_behavior():
 	# set player position and velocity to that of the carousel so you fly off
 	player.rotation.x = 0
