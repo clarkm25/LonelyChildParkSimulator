de4e9d046992d4caef28523aea70c50347bd2a00
arcangel25 <97647934+arcangel25@users.noreply.github.com>
2024-10-28 17:52:22 -0700

    3pts per 2 seconds on soccerball

--- scenes/playground/soccerball/soccerball.gd
+++ scenes/playground/soccerball/soccerball.gd
@@ -16,7 +16,7 @@ func _process(delta: float) -> void:
 func _on_rigid_body_3d_body_entered(body: Node) -> void:
 	if body is CharacterBody3D and interactable:
 		interactable = false
-		get_tree().get_first_node_in_group("player").happiness += 5
+		get_tree().get_first_node_in_group("player").happiness += 3
 		$Timer.start()
 		
 
